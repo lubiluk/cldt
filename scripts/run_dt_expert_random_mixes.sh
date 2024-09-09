e73a7227d9b09a5543a6fe8383f7b1f0cadb0515
Pawel Gajewski <lubiluk@gmail.com>
2024-09-09 14:15:10 +0200

    run scripts

---
+++ scripts/run_dt_expert_random_mixes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if seed is passed as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <seed>"
+    exit 1
+fi
+
+# Seed from the command line argument
+SEED="$1"
+
+for task in reach push pick_and_place
+do
+    for type in sparse dense
+    do
+        for ratio in 1_0 0_75 0_5 0_25 0_0
+        do 
+            ./spython.sh train_single.py -c configs/dt_panda_${task}_${type}_tf.yaml --dataset panda_${task}_${type}_1m_expert_ratio_${ratio} -save-path panda_${task}_${type}_1m_expert_ratio_${ratio} --seed ${SEED}
+        done
+    done
+done
+
